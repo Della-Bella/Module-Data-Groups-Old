dbf5c976840437d4bb09fa33a7ca2f9a05b458d4
Della Bella <gisdellabella@uol.com.br>
2025-11-16 12:54:47 +0000

    Slide show stage 1

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -4,5 +4,29 @@ const images = [
     "./assets/cute-cat-c.jpg",
 ];
 
+let currentIndex = 0; 
+const carouselImg = document.getElementById("carousel-img"); 
+const backwardBtn = document.getElementById("backward-btn"); 
+const forwardBtn = document.getElementById("forward-btn"); 
 
-// Write your code here
+
+function updateImage() {
+    carouselImg.src = images[currentIndex];
+}
+
+forwardBtn.addEventListener("click", () => {
+    currentIndex++; 
+    if (currentIndex >= images.length) {
+        currentIndex = 0; 
+    }
+    updateImage(); 
+});
+backwardBtn.addEventListener("click", () => {
+    currentIndex--; 
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1; 
+    }
+    updateImage(); 
+});
+
+updateImage();

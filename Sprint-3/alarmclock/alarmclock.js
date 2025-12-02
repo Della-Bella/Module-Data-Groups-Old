f7c147f08be1819802870e98cef11f51e71490ef
Della Bella <gisdellabella@uol.com.br>
2025-12-02 08:36:18 +0000

    Alarm Clock Start / get input

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,17 @@
-function setAlarm() {}
+function setAlarm() {
+  // fist need to get the input from the id=alarmset
+  const setTimeSeconds = document.getElementById("alarmSet").value;
+  console.log(setTimeSeconds);
+  const minutes = Math.trunc(setTimeSeconds/60);
+  const seconds = setTimeSeconds%60;
+  console.log(minutes);
+  // then update time remaining whith the value of alarm set
+  const upDate = document.getElementById("timeRemaining");
+  // Time Remaining: 00:00
+  upDate.innerHTML = `Time Remaining: ${minutes
+    .toString()
+    .padStart(2, "0")}: ${seconds.toString().padStart(2, "0")}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -8,6 +21,7 @@ function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
+// need tomake the functon counting donw seconds by one until get 00:00
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
--- Sprint-3/alarmclock/readme.md
+++ Sprint-3/alarmclock/readme.md
@@ -7,7 +7,7 @@ You will need to write your implementation in `alarmclock.js`.
 
 ## How the clock should work
 
-When you click the `Set Alarm` button the counter at the top of the screen should change to the number you entered in the `input` field. For example, if the `input` field says `10` then the title should say `Time Remaining: 00:10`.
+<!-- When you click the `Set Alarm` button the counter at the top of the screen should change to the number you entered in the `input` field. For example, if the `input` field says `10` then the title should say `Time Remaining: 00:10`. -->
 
 Every one second the title should count down by one.
 

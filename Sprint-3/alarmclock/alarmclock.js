88e8a4b41a198d7daab2fe4969ee5d1fad98af12
Della Bella <gisdellabella@uol.com.br>
2025-12-15 23:23:06 +0000

    Check alarm get to Zero/ Counting donw time

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,16 +1,30 @@
+let myTimer;
+
 function setAlarm() {
   // fist need to get the input from the id=alarmset
-  const setTimeSeconds = document.getElementById("alarmSet").value;
+  let setTimeSeconds = document.getElementById("alarmSet").value;
   console.log(setTimeSeconds);
-  const minutes = Math.trunc(setTimeSeconds/60);
-  const seconds = setTimeSeconds%60;
-  console.log(minutes);
-  // then update time remaining whith the value of alarm set
-  const upDate = document.getElementById("timeRemaining");
-  // Time Remaining: 00:00
-  upDate.innerHTML = `Time Remaining: ${minutes
-    .toString()
-    .padStart(2, "0")}: ${seconds.toString().padStart(2, "0")}`;
+
+  myTimer = setInterval(() => {
+    const minutes = Math.trunc(setTimeSeconds / 60);
+    const seconds = setTimeSeconds % 60;
+    console.log(minutes);
+    // then update time remaining whith the value of alarm set
+    const upDate = document.getElementById("timeRemaining");
+    // Time Remaining: 00:00
+    upDate.innerHTML = `Time Remaining: ${minutes
+      .toString()
+      .padStart(2, "0")}: ${seconds.toString().padStart(2, "0")}`;
+
+    //  Check if we get to zero
+    if (setTimeSeconds <= 0) {
+      playAlarm();
+      clearInterval(myTimer);
+    }
+
+    // need tomake the functon counting donw seconds by one until get 00:00
+    setTimeSeconds = setTimeSeconds - 1;
+  }, 1000);
 }
 
 // DO NOT EDIT BELOW HERE
@@ -21,10 +35,12 @@ function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-// need tomake the functon counting donw seconds by one until get 00:00
+
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
+    //Stop the countdown when stop is clicked
+    clearInterval(myTimer);
   });
 }
 
